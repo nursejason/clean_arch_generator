d8d569c3610dbc05749e4886021a2b442e1e69a9
Jason 'Princess' Job <jason_job@cable.comcast.com>
2016-07-13 19:15:44 -0600

    Working create_prject_dir

--- clean_arch_generator/application.py
+++ clean_arch_generator/application.py
@@ -7,17 +7,18 @@ def create_clean_project(context, **kwargs):
         lib_path = create_project_dir(
             context, kwargs['base'], kwargs['parent'], kwargs['child'])
 
-    create_project_files(kwargs['base'], kwargs['parent'], kwargs['child'])
+    #create_project_files(kwargs['base'], kwargs['parent'], kwargs['child'])
 
-    add_adapter_classes(lib_path, kwargs.get('adapter', []))
-    add_handler_classes(lib_path, kwargs.get('handler', []))
-    add_domain_classes(lib_path, kwargs.get('domain', []))
+    #add_adapter_classes(lib_path, kwargs.get('adapter', []))
+    #add_handler_classes(lib_path, kwargs.get('handler', []))
+    #add_domain_classes(lib_path, kwargs.get('domain', []))
 
 def create_project_dir(context, base_repo, parent_project, child_project):
     """ Create the new project directory """
     path = '%s/lib/%s/' % (base_repo, parent_project)
     if child_project is not None:
         path = path + '%s/' % child_project
+    print 'Creating directories. New lib path %s.' % path
     context.create_directory(path)
 
     return path
