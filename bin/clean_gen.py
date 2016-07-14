f9776e56269c93be32a831fd2de0913ef3899dd3
Fekete <kimberly_fekete@cable.comcast.com>
2016-07-13 18:39:47 -0600

    call main

--- bin/clean_gen.py
+++ bin/clean_gen.py
@@ -1,4 +1,9 @@
 import argparse
+import os
+import sys
+
+sys.path.append(os.path.realpath('..'))
+from clean_arch_generator.main import main
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -9,11 +14,14 @@ if __name__ == "__main__":
     parser.add_argument("--child", action="store",
                         dest="child", help="Child project name")
     parser.add_argument("--domain", action="store",
-                        dest="domain", help="Domain classes")
-    parser.add_argument("--adapter", action="store",
+                        dest="domain", help="Domain classes", default=[])
+    parser.add_argument("--adapter", action="store", default=[],
                         dest="adapter", help="Services being used i.e MySQL")
     parser.add_argument("--handler", action="store",
-                        dest="handler", help="Handler classes")
+                        dest="handler", help="Handler classes", default=[])
     parser.add_argument("-flask", action="store_true",
                         dest="flask", help="Add Flask API bootstrapping")
+    parser.add_argument("-new", action="store_true",
+                        dest="new", help="Create new directory")
     args = parser.parse_args()
+    main(**args.__dict__)
