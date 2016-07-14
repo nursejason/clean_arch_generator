54a150065dd0301bb122e2561ff8e454531c65d5
Jason 'Princess' Job <jason_job@cable.comcast.com>
2016-07-13 18:44:43 -0600

    Send OsAdapter to app layer

--- clean_arch_generator/main.py
+++ clean_arch_generator/main.py
@@ -1,5 +1,6 @@
 """ Main pass through to call app """
 from clean_arch_generator.application import create_clean_project
+from clean_arch_generator.adapters import OsAdapter
 
 def main(**kwargs):
-    create_clean_project(**kwargs)
+    create_clean_project(OsAdapter(), **kwargs)
